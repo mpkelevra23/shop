9aaff3aaa9dddf92602b22364ff9e8f3f89fa7dc
mpkelevra23 <mpkelevra23@gmail.com>
2019-07-18 13:52:05 +0300

    Прописал новые роуты

--- app/web/config/routes.php
+++ app/web/config/routes.php
@@ -9,12 +9,19 @@ return array(
     'category/([0-9]+)/page-([0-9]+)' => 'catalog/category/$1/$2',  // actionCategory в CatalogController
     'category/([0-9]+)' => 'catalog/category/$1',  // actionCategory в CatalogController
 
+    'cart/add/([0-9]+)' => 'cart/add/$1', //actionAdd в CartController
+
+    'cart/addAjax/([0-9]+)' => 'cart/addAjax/$1', // actionAdd в CartController
+    'cart' => 'cart/index', // actionIndex в CartController
+
     'user/registration' => 'user/registration', // actionRegistration в UserController
     'user/login' => 'user/login', // actionLogin в UserController
     'user/logout' => 'user/logout', // actionLogout в UserController
 
     'cabinet/edit' => 'cabinet/edit', // actionEdit в CabinetController
     'cabinet' => 'cabinet/index', // actionIndex в CabinetController
+
+    'contacts' => 'site/contact', //actionContact в SiteController
 
     '' => 'site/index', // actionIndex в SiteController
 

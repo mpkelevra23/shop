8f3c2cd9f4e09fc642663c0b665b04ded6adce95
mpkelevra23 <mpkelevra23@gmail.com>
2019-07-18 13:55:35 +0300

    Добавлена форма обратной связи

--- app/web/controllers/SiteController.php
+++ app/web/controllers/SiteController.php
@@ -14,4 +14,42 @@ class SiteController
         return true;
     }
 
+    /*
+     * Надо подключить отправку сообщений
+     */
+
+    public function actionContact()
+    {
+
+        $userEmail = '';
+        $userText = '';
+        $result = false;
+
+        if (isset($_POST['submit'])) {
+
+            $userEmail = $_POST['userEmail'];
+            $userText = $_POST['userText'];
+
+            $errors = false;
+
+            // Валидация полей
+            if (!User::checkEmail($userEmail)) {
+                $errors[] = 'Неправильный email';
+            }
+
+            if ($errors == false) {
+                $adminEmail = 'mpkelevra23@gmail.com';
+                $message = "Текст: {$userText}. От {$userEmail}";
+                $subject = 'Тема письма';
+                $result = mail($adminEmail, $subject, $message);
+                $result = true;
+            }
+
+        }
+
+        require_once(ROOT . '/views/site/contact.php');
+
+        return true;
+    }
+
 }
